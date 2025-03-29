87ee01407791fd854e0c1cda53d333243974f8fe
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-03-29 12:53:08 +0100

    add player sprite

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"embed"
+	"image"
+	_ "image/png"
 	"log"
+	"m3g4p0p/game/util"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+//go:embed assets/*
+var assets embed.FS
+var playerSprite = util.Must(loadImage("assets/playerShip1_blue.png"))
+
+func loadImage(name string) (*ebiten.Image, error) {
+	f, err := assets.Open(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	if img, _, err := image.Decode(f); err != nil {
+		return nil, err
+	} else {
+		return ebiten.NewImageFromImage(img), nil
+	}
+
+}
 
 type Game struct{}
 
@@ -14,7 +38,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.DrawImage(playerSprite, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
