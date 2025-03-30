0fbf11ffd9d8c481a3a6114f7b5066ba51294157
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-03-30 09:23:36 +0200

    add target position

--- main.go
+++ main.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"log"
 	"m3g4p0p/game/util"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 //go:embed assets/*
 var assets embed.FS
 var playerSprite = util.Must(util.LoadImage(assets, "assets/playerShip1_blue.png"))
 
-type Game struct{}
+type Player struct {
+	pos *ebiten.GeoM
+}
+
+type Game struct {
+	player    *Player
+	targetPos *ebiten.GeoM
+}
 
 func (g *Game) Update() error {
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		g.targetPos.Reset()
+		g.targetPos.Translate(float64(x), float64(y))
+	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	x, y := ebiten.CursorPosition()
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(float64(x), float64(y))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("%+v", g.player.pos))
+	op := &ebiten.DrawImageOptions{GeoM: *g.targetPos}
 	screen.DrawImage(playerSprite, op)
 }
 
@@ -33,7 +46,9 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	game := &Game{player: &Player{&ebiten.GeoM{}}, targetPos: &ebiten.GeoM{}}
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
