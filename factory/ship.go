59f28c13825fabaf62ce8ab9a8ad4b83ee686997
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-04-05 16:58:47 +0200

    add factory package

---
+++ factory/ship.go
@@ -0,0 +1,29 @@
+package factory
+
+import (
+	"m3g4p0p/game/components"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/yohamta/donburi"
+	"github.com/yohamta/donburi/features/math"
+	"github.com/yohamta/donburi/features/transform"
+)
+
+func CreateShip(world donburi.World, sprite *ebiten.Image, x, y float64) *donburi.Entry {
+	ship := world.Entry(world.Create(transform.Transform, components.Sprite))
+	transform.Transform.SetValue(ship, transform.TransformData{
+		LocalPosition: math.Vec2{X: x, Y: y},
+	})
+	components.Sprite.Set(ship, sprite)
+	return ship
+}
+
+func CreateFire(world donburi.World, sprite *ebiten.Image, ship *donburi.Entry) *donburi.Entry {
+	fire := world.Entry(world.Create(transform.Transform, components.Sprite))
+	transform.Transform.SetValue(fire, transform.TransformData{
+		LocalPosition: math.Vec2{X: 0, Y: float64(sprite.Bounds().Dy())},
+	})
+	components.Sprite.Set(fire, sprite)
+	transform.SetParent(fire, ship, false)
+	return fire
+}
